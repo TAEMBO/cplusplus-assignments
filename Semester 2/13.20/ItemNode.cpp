7e1e26fa8eb5588cd27914a80c8b4562e64e2869
TÆMBØ <kuneshr123@gmail.com>
2024-04-28 21:37:39 -0700

    Add files

---
+++ Semester 2/13.20/ItemNode.cpp
@@ -0,0 +1,51 @@
+#include "ItemNode.h"
+
+// Constructor
+ItemNode::ItemNode() {
+    item = "";
+    nextNodeRef = NULL;
+}
+
+// Constructor                                                                                     
+ItemNode::ItemNode(string itemInit) {
+    item = itemInit;
+    nextNodeRef = NULL;
+}
+
+// Constructor        
+ItemNode::ItemNode(string itemInit, ItemNode *nextLoc) {
+    item = itemInit;
+    nextNodeRef = nextLoc;
+}
+
+// Insert node after this node.     
+void ItemNode::InsertAfter(ItemNode &nodeLoc) {
+    ItemNode* tmpNext;
+    
+    tmpNext = nextNodeRef;
+    nextNodeRef = &nodeLoc;
+    nodeLoc.nextNodeRef = tmpNext;
+}
+
+// TODO: Define the InsertAtEnd() member function that inserts a parameter ItemNode
+//       to the end of the linked list
+void ItemNode::InsertAtEnd(ItemNode* nodeLoc) {
+    
+    nextNodeRef = nodeLoc;
+    nodeLoc->nextNodeRef = nullptr;
+}
+
+// Get location pointed by nextNodeRef                                                             
+ItemNode* ItemNode::GetNext() {
+    return nextNodeRef;
+}
+
+// Print data of this node
+void ItemNode::PrintNodeData() {
+    cout << item << endl;
+}
+
+// Get data of this node
+string ItemNode::GetNodeItem() {
+    return item;
+}
