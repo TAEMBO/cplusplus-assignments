c10aa7d8870b2e3c3c6f3a92d354f67e35cbb463
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2024-08-29 21:33:37 -0700

    Create Semester 3 (200)

---
+++ Semester 3 (200)/A02.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        :
+// Author      :
+// Class       :
+// Assignment  : 0.2
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Let's play computer with this" << endl;
+
+	return 0;
+}
